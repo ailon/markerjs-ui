34f68412f02f58d3d1ac42b3fb2fc3480dd20f22
Alan Mendelevich <ailon@ailon.org>
2025-09-11 10:00:34 +0300

    fix stroke style icons not visible in some setups

--- src/lib/StrokeToolboxPanel.ts
+++ src/lib/StrokeToolboxPanel.ts
@@ -123,7 +123,7 @@ export class StrokeToolboxPanel extends ToolboxPanel {
   }
 
   private getStrokeStyleVisual(strokeDasharray: string): string {
-    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><path stroke-dasharray="${strokeDasharray}" d="M2,12 H22" /></svg>`;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><path stroke-dasharray="${strokeDasharray}" d="M2,12 H22" /></svg>`;
   }
 
   private handleStrokeStyleClick(strokeDasharray: string) {
